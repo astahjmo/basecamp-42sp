ecf6c06530319e79ece2aeeeca6ab7051c30a670
Johnatan <contato.johnatan373@gmail.com>
2022-02-18 09:34:34 -0300

    a

--- shell01/C04/ex00/ft_strlen.c
+++ shell01/C04/ex00/ft_strlen.c
@@ -1,35 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: johmatos <johmatos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/12 14:35:12 by johmatos          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/02/16 19:34:05 by johmatos         ###   ########.fr       */
-=======
-/*   Updated: 2022/02/17 20:18:49 by johmatos         ###   ########.fr       */
->>>>>>> 15574a7 (att)
+/*   Created: 2022/02/17 14:04:28 by johmatos          #+#    #+#             */
+/*   Updated: 2022/02/17 14:05:36 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcpy(char *dest, char *src)
+int	ft_strlen(char *str)
 {
-	int		index;
+	int	index;
 
 	index = 0;
-<<<<<<< HEAD
-	while (*src)
-	{
-		dest[index] = *src;
-		src++;
-=======
-	while (src[index])
-	{
-		dest[index] = src[index];
->>>>>>> 15574a7 (att)
+	while (str[index])
 		index++;
-	}
-	return (dest);
+	return (index);
 }
--- shell01/C04/ex01/ft_putstr.c
+++ shell01/C04/ex01/ft_putstr.c
@@ -1,35 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: johmatos <johmatos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/12 14:35:12 by johmatos          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/02/16 19:34:05 by johmatos         ###   ########.fr       */
-=======
-/*   Updated: 2022/02/17 20:18:49 by johmatos         ###   ########.fr       */
->>>>>>> 15574a7 (att)
+/*   Created: 2022/02/17 14:06:18 by johmatos          #+#    #+#             */
+/*   Updated: 2022/02/17 14:07:36 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcpy(char *dest, char *src)
+#include <unistd.h>
+
+void	ft_putstr(char *str)
 {
-	int		index;
+	int	index;
 
 	index = 0;
-<<<<<<< HEAD
-	while (*src)
-	{
-		dest[index] = *src;
-		src++;
-=======
-	while (src[index])
-	{
-		dest[index] = src[index];
->>>>>>> 15574a7 (att)
-		index++;
-	}
-	return (dest);
+	while (str[index])
+		write(1, &str[index], 1);
 }
